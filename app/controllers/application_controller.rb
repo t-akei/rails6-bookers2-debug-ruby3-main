b685786b74ff72d670b5ddb20b7d1e1654d5d13f
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-13 04:11:03 +0000

    [Finish]応用課題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    # user_path(current_user)
+      user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
