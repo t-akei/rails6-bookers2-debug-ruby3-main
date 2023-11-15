f24db5559cca989c73ec99a11759ccc04090850c
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-15 09:20:52 +0000

    [応用課題3]

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,14 @@
+class FavoritesController < ApplicationController
+  
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_back(fallback_location: books_path)
+  end
+  
+  def destroy
+  end
+  
+  
+end
