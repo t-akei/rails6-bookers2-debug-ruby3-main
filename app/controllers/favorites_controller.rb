05e8d9f885653dc81387b0379a2ba3b3d6d99fb9
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-18 06:49:17 +0000

    [応用課題3]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,6 +5,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
     redirect_back(fallback_location: books_path)
+    # redirect_to request.referer
   end
   
   def destroy
@@ -12,6 +13,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_back(fallback_location: books_path)
+    # redirect_to request.referer
   end
   
   
